6a45c89e07d8c7861e95f8de1059fc69ce6b69dc
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-06-30 20:56:48 +0900

    Chore: 프로젝트 생성 #1

---
+++ GAM/GAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

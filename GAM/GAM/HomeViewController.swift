54f92efac66b7d5099e9a1c9e80efb2637d42544
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-06-30 21:04:19 +0900

    Chore: 코드베이스 프로젝트 세팅 #1

---
+++ GAM/GAM/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/06/30.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        debugPrint("HomeViewController")
+    }
+}

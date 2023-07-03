b039cb4a98c214dc161250c37c39ef2bd3494bd8
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-03 12:16:18 +0900

    Chore: BaseViewController 기능 구현 #7

--- GAM/GAM/Global/Bases/BaseViewController.swift
+++ GAM/GAM/Global/Bases/BaseViewController.swift
@@ -11,12 +11,12 @@ class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: Properties
     
-//    lazy var activityIndicator: SnapfitActivityIndicatorView = {
-//        let activityIndicator: SnapfitActivityIndicatorView = SnapfitActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//        activityIndicator.center = self.view.center
-//
-//        return activityIndicator
-//    }()
+    lazy var activityIndicator: GamActivityIndicatorView = {
+        let activityIndicator: GamActivityIndicatorView = GamActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        activityIndicator.center = self.view.center
+        
+        return activityIndicator
+    }()
     
     let screenWidth = UIScreen.main.bounds.size.width
     let screenHeight = UIScreen.main.bounds.size.height
@@ -26,24 +26,24 @@ class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.setBackgroundColor()
+        self.setBackgroundColor()
     }
 }
 
 // MARK: Methods
 
 extension BaseViewController {
-//    func hideTabBar() {
-//        if let tabBarController = self.tabBarController as? SnapfitTabBarController {
-//            tabBarController.hideTabBar()
-//        }
-//    }
-//
-//    func showTabBar() {
-//        if let tabBarController = self.tabBarController as? SnapfitTabBarController {
-//            tabBarController.showTabBar()
-//        }
-//    }
+    func hideTabBar() {
+        if let tabBarController = self.tabBarController as? GamTabBarController {
+            tabBarController.hideTabBar()
+        }
+    }
+    
+    func showTabBar() {
+        if let tabBarController = self.tabBarController as? GamTabBarController {
+            tabBarController.showTabBar()
+        }
+    }
     
     /// 화면 터치 시 키보드 내리는 메서드
     func hideKeyboardWhenTappedAround() {
@@ -53,29 +53,29 @@ extension BaseViewController {
         view.addGestureRecognizer(tap)
     }
     
-//    /// 모든 뷰의 기본 Background color 설정
-//    private func setBackgroundColor() {
-//        self.view.backgroundColor = .sfGrayWhite
-//    }
-//
-//    /// 서버 통신 시작 시 Activity Indicator를 시작하는 메서드
-//    func startActivityIndicator() {
-//        self.view.addSubview(self.activityIndicator)
-//        self.activityIndicator.startAnimating()
-//    }
-//
-//    /// 서버 통신이 끝나면 Activity Indicator를 종료하는 메서드
-//    func stopActivityIndicator() {
-//        self.activityIndicator.stopAnimating()
-//        self.activityIndicator.removeFromSuperview()
-//    }
-//
-//    /// BackButton에 pop Action을 간편하게 주는 메서드.
-//    /// - 필요 시 override하여 사용
-//    @objc
-//    func setBackButtonAction(_ button: UIButton) {
-//        button.setAction { [weak self] in
-//            self?.navigationController?.popViewController(animated: true)
-//        }
-//    }
+    /// 모든 뷰의 기본 Background color 설정
+    private func setBackgroundColor() {
+        self.view.backgroundColor = .gamGray1
+    }
+    
+    /// 서버 통신 시작 시 Activity Indicator를 시작하는 메서드
+    func startActivityIndicator() {
+        self.view.addSubview(self.activityIndicator)
+        self.activityIndicator.startAnimating()
+    }
+    
+    /// 서버 통신이 끝나면 Activity Indicator를 종료하는 메서드
+    func stopActivityIndicator() {
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.removeFromSuperview()
+    }
+    
+    /// BackButton에 pop Action을 간편하게 주는 메서드.
+    /// - 필요 시 override하여 사용
+    @objc
+    func setBackButtonAction(_ button: UIButton) {
+        button.setAction { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+    }
 }

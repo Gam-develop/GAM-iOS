0d8fe7453338a2023c602f5f692eb9a586e921d4
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-07 17:52:36 +0900

    Add: ChevronLeft icon #9

--- GAM/GAM/Global/Extensions/UIImage+.swift
+++ GAM/GAM/Global/Extensions/UIImage+.swift
@@ -12,6 +12,7 @@ extension UIImage {
     static let kakaoLoginMediumWide: UIImage = UIImage(named: "kakao_login_medium_wide") ?? UIImage()
     static let gamLogoKorean: UIImage = UIImage(named: "gamLogoKorean") ?? UIImage()
     static let textFieldClear: UIImage = (UIImage(named: "textFieldClear") ?? UIImage()).withRenderingMode(.alwaysOriginal)
+    static let chevronLeft: UIImage = (UIImage(named: "typeChevronLeft") ?? UIImage()).withRenderingMode(.alwaysOriginal)
     
     func resizeWithWidth(width: CGFloat) -> UIImage? {
         let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))))

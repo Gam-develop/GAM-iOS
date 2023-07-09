c1ad1b155a3173f80f19adf1f386b0a5f6a2c711
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-09 18:43:34 +0900

    Chore: UILabel+ 행간 포함한 스타일 설정 메서드 구현 #10

--- GAM/GAM/Global/Extensions/UILabel+.swift
+++ GAM/GAM/Global/Extensions/UILabel+.swift
@@ -65,6 +65,40 @@ extension UILabel {
         }
     }
     
+    func setTextWithStyle(to targetString: String, style: UIFont, color: UIColor) {
+        let text = targetString
+        let attributedString = NSMutableAttributedString(string: text)
+        
+        attributedString.addAttribute(
+            .font,
+            value: style,
+            range: (text as NSString).range(of: targetString)
+        )
+        
+        attributedString.addAttribute(
+            .foregroundColor,
+            value: color,
+            range: (text as NSString).range(of: targetString)
+        )
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        switch style {
+        case .headline4Bold:
+            paragraphStyle.lineSpacing = style.pointSize * 0.3
+        default:
+            paragraphStyle.lineSpacing = style.pointSize * 0.48
+        }
+        
+        attributedString.addAttribute(
+            .paragraphStyle,
+            value: paragraphStyle,
+            range: NSRange(location: 0, length: attributedString.length)
+        )
+        
+        self.attributedText = attributedString
+    }
+    
     func setHyperlinkedStyle(to targetStrings: [String], with font: UIFont
     ) {
         if let labelText = self.text, labelText.count > 0 {

bbfe27095852beee2b2a0f0668fc88647ec27543
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-09 18:42:11 +0900

    Chore: MagazineEntity image url로 받도록 변경 #10

--- GAM/GAM/Sources/Entities/MagazineEntity.swift
+++ GAM/GAM/Sources/Entities/MagazineEntity.swift
@@ -9,16 +9,16 @@ import UIKit
 
 struct MagazineEntity {
     let id: Int
-    let thumbnailImage: UIImage
+    let thumbnailImageURL: String
     let title: String
     let author: String
     var isScrap: Bool
     let url: String
     var visibilityCount: Int
     
-    init(id: Int, thumbnailImage: UIImage, title: String, author: String, isScrap: Bool, url: String, visibilityCount: Int) {
+    init(id: Int, thumbnailImageURL: String, title: String, author: String, isScrap: Bool, url: String, visibilityCount: Int) {
         self.id = id
-        self.thumbnailImage = thumbnailImage
+        self.thumbnailImageURL = thumbnailImageURL
         self.title = title
         self.author = author
         self.isScrap = isScrap

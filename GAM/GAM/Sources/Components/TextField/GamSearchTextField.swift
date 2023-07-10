12b80c525792f27f0a63d62cc5b727fa26177830
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-10 13:42:34 +0900

    Feat: GamSearchTextField 구현 #13

---
+++ GAM/GAM/Sources/Components/TextField/GamSearchTextField.swift
@@ -0,0 +1,102 @@
+//
+//  GamSearchTextField.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/07/10.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class GamSearchTextField: UITextField {
+    
+    enum Text {
+        static let placeholder = "검색 플레이스홀더 있으면 좋음"
+    }
+    
+    // MARK: UIComponents
+    
+    private let leadingView: UIView = UIView(frame: CGRect(x: 4, y: 0, width: 46, height: 44))
+    
+    private let searchImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView(image: .icnSearch)
+        imageView.frame = CGRect(x: 4, y: 0, width: 44, height: 44)
+        return imageView
+    }()
+    
+    let clearButton: UIButton = {
+        let button: UIButton = UIButton(type: .system)
+        button.setImage((.textFieldClear), for: .normal)
+        button.isHidden = true
+        return button
+    }()
+    
+    // MARK: Properties
+    
+    private var disposeBag: DisposeBag = DisposeBag()
+    
+    // MARK: Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setUI()
+        self.setLayout()
+        self.setClearButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Methods
+    
+    private func setClearButton() {
+        self.clearButton.rx.tap
+            .bind {
+                self.text = ""
+                self.clearButton.isHidden = true
+            }
+            .disposed(by: self.disposeBag)
+        
+        self.rx.text
+            .orEmpty
+            .distinctUntilChanged()
+            .subscribe(onNext: { changedText in
+                self.clearButton.isHidden = changedText != "" ? false : true
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func setPlaceholderWithColor(placeholder: String) {
+        self.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.gamGray2])
+    }
+}
+
+// MARK: - UI
+
+extension GamSearchTextField {
+    private func setUI() {
+        self.backgroundColor = .gamWhite
+        self.makeRounded(cornerRadius: 8)
+        self.setPlaceholderWithColor(placeholder: Text.placeholder)
+        self.font = .body2Medium
+        self.leftViewMode = .always
+        self.tintColor = .gamBlack
+        self.returnKeyType = .search
+        self.addRightPadding(44)
+    }
+    
+    private func setLayout() {
+        self.leadingView.addSubview(searchImageView)
+        self.leftView = self.leadingView
+        self.addSubview(clearButton)
+        
+        self.clearButton.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+            make.width.equalTo(self.clearButton.snp.height)
+        }
+    }
+}

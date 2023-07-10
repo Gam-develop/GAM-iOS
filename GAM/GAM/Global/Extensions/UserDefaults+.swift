e50ce4fbc01d2735c754703c1f8d11d7289da74c
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-10 19:35:57 +0900

    Chore: UserDefualts Key 관리 방식 rawValue로 변경 #13

--- GAM/GAM/Global/Extensions/UserDefaults+.swift
+++ GAM/GAM/Global/Extensions/UserDefaults+.swift
@@ -10,7 +10,8 @@ import Foundation
 extension UserDefaults {
     
     /// UserDefaults key value가 많아지면 관리하기 어려워지므로 enum 'Keys'로 묶어 관리합니다.
-    enum Keys {
-        static var userID = "userID"
+    enum Keys: String {
+        case userID = "userID"
+        case recentSearch = "recentSearch"
     }
 }
--- GAM/GAM/Global/Utils/UserDefaultsManager.swift
+++ GAM/GAM/Global/Utils/UserDefaultsManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct UserDefaultsManager {
     static var userID: Int? {
-        get { return UserDefaults.standard.integer(forKey: UserDefaults.Keys.userID) }
-        set { UserDefaults.standard.set(newValue, forKey: UserDefaults.Keys.userID) }
+        get { return UserDefaults.standard.integer(forKey: UserDefaults.Keys.userID.rawValue) }
+        set { UserDefaults.standard.set(newValue, forKey: UserDefaults.Keys.userID.rawValue) }
     }
 }

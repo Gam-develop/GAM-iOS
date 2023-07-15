8ac5ffc0ada77ef6dc50b4d7c0145e5e9eeeda04
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-15 19:54:02 +0900

    Create: MypageViewController 생성 #17

--- GAM/GAM/Sources/Screens/Mypage/MypageViewController.swift
+++ GAM/GAM/Sources/Screens/Mypage/MypageViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Jungbin on 2023/07/01.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MypageViewController: BaseViewController {
+    
+    // MARK: UIComponents
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setLayout()
+    }
+    
+    // MARK: Methods
+}
+
+// MARK: - UI
+
+extension MypageViewController {
+    private func setLayout() {
+        self.view.addSubviews([])
+    }
+}

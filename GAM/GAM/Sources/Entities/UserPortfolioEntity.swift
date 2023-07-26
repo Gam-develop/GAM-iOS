3fd043a4f4f6598d6ca82b03a117c6e777f40b1c
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-07-27 00:35:05 +0900

    Chore: 유저 페이지에 필요한 entity 구성 #19

---
+++ GAM/GAM/Sources/Entities/UserPortfolioEntity.swift
@@ -0,0 +1,20 @@
+//
+//  UserPortfolioEntity.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/07/26.
+//
+
+import Foundation
+
+struct UserPortfolioEntity: Hashable {
+    let id: Int
+    let behanceURL: String
+    let instagramURL: String
+    let notionURL: String
+    let works: [WorkEntity]
+    
+    static func == (lhs: UserPortfolioEntity, rhs: UserPortfolioEntity) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
---
+++ GAM/GAM/Sources/Entities/WorkEntity.swift
@@ -0,0 +1,15 @@
+//
+//  WorkEntity.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/07/27.
+//
+
+import Foundation
+
+struct WorkEntity: Hashable {
+    let id: Int
+    let thumbnailImageURL: String
+    let title: String
+    let detail: String
+}

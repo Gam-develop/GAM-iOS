ed72713e1226e2d63666ebbffd901d73ff1fd37a
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-08-02 00:23:18 +0900

    Chore: Message text 추가 #19

--- GAM/GAM/Global/Utils/Message.swift
+++ GAM/GAM/Global/Utils/Message.swift
@@ -7,6 +7,13 @@
 
 enum Message {
     case networkError
+    case unabledMailApp
+    case completedSendContactMail
+    case failedSendContactMail
+    case completedPaste
+    case completedUserBlock
+    case completedUserReport
+    case completedRemove
 }
 
 extension Message {
@@ -19,6 +26,22 @@ extension Message {
 네트워크 오류로 인해 연결에 실패하였습니다.
 잠시 후에 다시 시도해 주세요.
 """
+        case .unabledMailApp:
+            return "Mail 앱을 사용할 수 없습니다. 기기에 Mail 앱이 설치되어 있는지 확인해 주세요."
+            
+        case .completedSendContactMail:
+            return "메일 전송이 완료되었습니다."
+            
+        case .failedSendContactMail:
+            return "메일 전송에 실패하였습니다. 다시 시도해 주세요."
+        case .completedPaste:
+            return "✉️ 복사했어요"
+        case .completedUserBlock:
+            return "🚫 차단되었어요"
+        case .completedUserReport:
+            return "🚨 신고되었어요"
+        case .completedRemove:
+            return "🗑️ 삭제되었어요"
         }
     }
 }

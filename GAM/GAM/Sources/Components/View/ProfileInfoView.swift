d6287cc596951420347c01d1deccff0187f2d98b
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-08-01 22:00:41 +0900

    Feat: ProfileInfoView 구현 #19

---
+++ GAM/GAM/Sources/Components/View/ProfileInfoView.swift
@@ -0,0 +1,87 @@
+//
+//  ProfileInfoView.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/08/01.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileInfoView: UIView {
+    
+    // MARK: UIComponents
+    
+    let infoTextField: UITextField = {
+        let textField: UITextField = UITextField()
+        textField.textColor = .gamBlack
+        textField.font = .subhead4Bold
+        return textField
+    }()
+    
+    private let underlineView: UIView = {
+        let view: UIView = UIView()
+        view.backgroundColor = .gamGray2
+        return view
+    }()
+    
+    let detailTextView: UITextView = {
+        let textView: UITextView = UITextView()
+        textView.font = .caption2Regular
+        textView.textColor = .gamBlack
+        textView.textContainerInset = .zero
+        textView.contentInset = .zero
+        return textView
+    }()
+    
+    // MARK: Initializer
+    
+    init(isEditable: Bool) {
+        super.init(frame: .zero)
+        
+        self.setUI(isEditable: isEditable)
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Methods
+    
+    func setData(info: String, detail: String) {
+        self.infoTextField.text = info
+        self.detailTextView.text = detail
+    }
+    
+    private func setUI(isEditable: Bool) {
+        self.backgroundColor = .gamWhite
+        self.makeRounded(cornerRadius: 8)
+        if !isEditable {
+            self.infoTextField.isUserInteractionEnabled = false
+            self.detailTextView.isEditable = false
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubviews([infoTextField, underlineView, detailTextView])
+        
+        self.infoTextField.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(20)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(27)
+        }
+        
+        self.underlineView.snp.makeConstraints { make in
+            make.top.equalTo(self.infoTextField.snp.bottom).offset(6)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(0.5)
+        }
+        
+        self.detailTextView.snp.makeConstraints { make in
+            make.top.equalTo(self.underlineView.snp.bottom).offset(12)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(24)
+        }
+    }
+}

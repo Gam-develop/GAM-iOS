ac3b3908de4b60b47b5733e6ca2cb6818eef813b
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-08-02 18:35:29 +0900

    Chore: SettingButton 구현 #26

---
+++ GAM/GAM/Sources/Components/Button/SettingButton.swift
@@ -0,0 +1,30 @@
+//
+//  SettingButton.swift
+//  GAM
+//
+//  Created by Jungbin on 2023/08/02.
+//
+
+import UIKit
+
+final class SettingButton: UIButton {
+    
+    // MARK: Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Methods
+    
+    private func setUI() {
+        self.tintColor = .clear
+        self.setImage(.icnSetting, for: .normal)
+    }
+}

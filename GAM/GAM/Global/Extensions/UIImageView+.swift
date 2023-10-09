7c44ed62b6c0a298dae3ccfdc3caf9274ce62161
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-10-09 20:57:07 +0900

    Fix: Image URL 인식 안 되던 문제 수정 #39

--- GAM/GAM/Global/Extensions/UIImageView+.swift
+++ GAM/GAM/Global/Extensions/UIImageView+.swift
@@ -18,7 +18,8 @@ extension UIImageView {
     
     /// URL을 통해 이미지를 불러오는 메서드 + 캐싱
     func setImageUrl(_ imageURL: String) {
-        let cacheKey = NSString(string: Environment.IMAGE_BASE_URL + imageURL)
+        let imageURL = Environment.IMAGE_BASE_URL + imageURL
+        let cacheKey = NSString(string: imageURL)
         
         /// 해당 Key에 캐시 이미지가 저장되어 있으면 이미지 사용
         if let cachedImage = ImageCacheManager.shared.object(forKey: cacheKey) {
@@ -30,7 +31,9 @@ extension UIImageView {
             let request = URLRequest(url: requestURL)
             
             URLSession.shared.dataTask(with: request) { data, response, error in
-                if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
+                if let data = data,
+                   let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300,
+                   let image = UIImage(data: data) {
                     
                     /// 다운받은 이미지를 캐시에 저장
                     ImageCacheManager.shared.setObject(image, forKey: cacheKey)

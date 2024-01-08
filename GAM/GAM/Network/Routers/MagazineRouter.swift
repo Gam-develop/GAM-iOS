ce41e6a5e1bb54d9ac487f4d0d6b29a73c6a03a0
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-09 02:14:28 +0900

    Chore: 매거진 검색 Router, Service 작성 #39

--- GAM/GAM/Network/Routers/MagazineRouter.swift
+++ GAM/GAM/Network/Routers/MagazineRouter.swift
@@ -13,6 +13,7 @@ enum MagazineRouter {
     case getAllMagazine
     case getScrapMagazine
     case requestScrapMagazine(data: ScrapMagazineRequestDTO)
+    case searchMagazine(data: String)
 }
 
 extension MagazineRouter: TargetType {
@@ -31,12 +32,14 @@ extension MagazineRouter: TargetType {
             return "/magazine/scraps"
         case .requestScrapMagazine:
             return "/magazine/scrap"
+        case .searchMagazine:
+            return "/magazine/search"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .getPopularMagazine, .getAllMagazine, .getScrapMagazine:
+        case .getPopularMagazine, .getAllMagazine, .getScrapMagazine, .searchMagazine:
             return .get
         case .requestScrapMagazine:
             return .post
@@ -53,12 +56,18 @@ extension MagazineRouter: TargetType {
                 "currentScrapStatus": data.currentScrapStatus
             ]
             return .requestParameters(parameters: body, encoding: JSONEncoding.prettyPrinted)
+            
+        case .searchMagazine(let data):
+            let body: [String : Any] = [
+                "keyword": data
+            ]
+            return .requestParameters(parameters: body, encoding: URLEncoding.queryString)
         }
     }
     
     var headers: [String: String]? {
         switch self {
-        case .getPopularMagazine, .getAllMagazine, .getScrapMagazine, .requestScrapMagazine:
+        case .getPopularMagazine, .getAllMagazine, .getScrapMagazine, .requestScrapMagazine, .searchMagazine:
             return [
                 "Content-Type": "application/json",
                 "Authorization": UserInfo.shared.accessToken

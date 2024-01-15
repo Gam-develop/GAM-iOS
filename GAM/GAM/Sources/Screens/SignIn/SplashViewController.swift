acb53711b6afcbb15cbecfea857179e4a31ec7fe
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-16 01:11:39 +0900

    Feat: SplashViewController 구현 #47

---
+++ GAM/GAM/Sources/Screens/SignIn/SplashViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SplashViewController.swift
+//  GAM
+//
+//  Created by Jungbin on 1/10/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SplashViewController: BaseViewController {
+    
+    // MARK: UIComponents
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: .gamLogoKorean)
+        return imageView
+    }()
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setUI()
+        self.setLayout()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0) {
+            self.autoSignIn()
+        }
+    }
+    
+    // MARK: Methods
+    
+    private func setUI() {
+        self.view.backgroundColor = .gamBlack
+    }
+    
+    
+    private func presentNextViewController(isProfileCompleted: Bool) {
+        if isProfileCompleted {
+            self.present(GamTabBarController(), animated: true)
+        } else {
+            self.present(BaseNavigationController(rootViewController: SignUpUsernameViewController()), animated: true)
+        }
+    }
+}
+
+// MARK: - UI
+
+extension SplashViewController {
+    private func setLayout() {
+        self.view.addSubviews([logoImageView])
+        
+        self.logoImageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.equalTo(110)
+            make.height.equalTo(75.74)
+        }
+    }
+}

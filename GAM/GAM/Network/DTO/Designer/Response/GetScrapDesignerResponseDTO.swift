515bf7d771b01526bfa0149f6f70cfc27f18d789
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-16 12:27:05 +0900

    Create: GetScrapDesignerResponseDTO 작성 #55

---
+++ GAM/GAM/Network/DTO/Designer/Response/GetScrapDesignerResponseDTO.swift
@@ -0,0 +1,36 @@
+//
+//  GetScrapDesignerResponseDTO.swift
+//  GAM
+//
+//  Created by Jungbin on 1/16/24.
+//
+
+import Foundation
+
+struct GetScrapDesignerResponseDTOElement: Codable {
+    let userID: Int
+    let userName: String
+    let userThumbNail: String
+    let userScrapID: Int
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case userScrapID = "userScrapId"
+        case userName, userThumbNail
+    }
+}
+
+typealias GetScrapDesignerResponseDTO = [GetScrapDesignerResponseDTOElement]
+
+extension GetScrapDesignerResponseDTO {
+    func toBrowseDesignerScrapEntity() -> [BrowseDesignerScrapEntity] {
+        return self.map { designerResponse in
+            BrowseDesignerScrapEntity(
+                id: designerResponse.userID,
+                thumbnailImageURL: designerResponse.userThumbNail,
+                name: designerResponse.userName,
+                isScrap: true
+            )
+        }
+    }
+}

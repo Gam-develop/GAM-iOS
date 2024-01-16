298b603db957cbb66561715a2f27fd56d6c659a5
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-16 12:34:44 +0900

    Chore: GetScrapDesignerResponseDTO Decodable로 수정 #55

--- GAM/GAM/Network/DTO/Designer/Response/GetScrapDesignerResponseDTO.swift
+++ GAM/GAM/Network/DTO/Designer/Response/GetScrapDesignerResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct GetScrapDesignerResponseDTOElement: Codable {
+struct GetScrapDesignerResponseDTOElement: Decodable {
     let userID: Int
     let userName: String
     let userThumbNail: String

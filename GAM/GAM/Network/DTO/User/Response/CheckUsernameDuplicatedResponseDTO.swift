638c99c5e0981be48ff46a6b7be43e357e8e9dc1
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-18 17:12:28 +0900

    Create: CheckUsernameDupliocatedResponseDTO 작성 #66

---
+++ GAM/GAM/Network/DTO/User/Response/CheckUsernameDuplicatedResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  CheckUsernameDuplicatedResponseDTO.swift
+//  GAM
+//
+//  Created by Jungbin on 1/18/24.
+//
+
+import Foundation
+
+struct CheckUsernameDuplicatedResponseDTO: Decodable {
+    let isDuplicated: Bool
+}

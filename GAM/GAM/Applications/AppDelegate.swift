afe1c53cce719dc967023cbb6807be5c0da9b6b2
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2024-01-24 17:10:03 +0900

    Chore: 토큰 등록, 갱신 관련 기능 구현 #16

--- GAM/GAM/Applications/AppDelegate.swift
+++ GAM/GAM/Applications/AppDelegate.swift
@@ -8,12 +8,39 @@
 import UIKit
 import KakaoSDKCommon
 import KakaoSDKAuth
+import Firebase
+import FirebaseCore
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         KakaoSDK.initSDK(appKey: Environment.KAKAO_NATIVE_APP_KEY)
+        
+        FirebaseApp.configure()
+        PushNotificationHelper.shared.setAuthorization()
+        // 앱 실행 시 사용자에게 알림 허용 권한 받기
+        UNUserNotificationCenter.current().delegate = self
+        
+        // 원격 알림 등록
+        application.registerForRemoteNotifications()
+
+        /// 메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        /// 자동 초기화 방지
+        Messaging.messaging().isAutoInitEnabled = true
+        
+        /// 현재 등록 토큰 가져오기
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+                UserDefaultsManager.fcmDeviceToken = token
+            }
+        }
+        
         return true
     }
     
@@ -37,5 +64,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         return false
+    }
+    
+    /// APN 토큰과 등록 토큰 매핑
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    /// APN 토큰과 등록 토큰 매핑 실패
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        debugPrint("APN 토큰 등록 실패", "fail")
+    }
+    
+    /// 디바이스 세로방향으로 고정
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    /// 토큰 갱신 모니터링 메서드
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        UserDefaultsManager.fcmDeviceToken = fcmToken
+        UserInfo.shared.deviceToken = fcmToken ?? "non-token"
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    /// foreGround에 푸시알림이 올 때 실행되는 메서드
+    func userNotificationCenter(_ center: UNUserNotificationCenter,willPresent notification: UNNotification,withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.badge, .sound, .list, .banner])
     }
 }
